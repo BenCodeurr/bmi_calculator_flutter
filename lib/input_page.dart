75eec4bf8ce7f5339d62d6d59510040c43a7347a
benCodeur <benedictobed20@gmail.com>
2024-01-13 16:54:12 +0000

    initial commit + app cards creation

---
+++ lib/input_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import './components/reusable_card.dart';
+
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerHeight = 80.0;
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('BMI CALCULATOR'),
+        ),
+      ),
+      body: Column(
+        children: [
+          const Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Expanded(
+            child: ReusableCard(
+              colour: activeCardColor,
+            ),
+          ),
+          const Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: const Color(0xFFEB1555),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          ),
+        ],
+      ),
+    );
+  }
+}
